6b7fb073ef0eb75c43e8b5e8cd2dbe7eeff11a5b
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-04 18:07:34 +0530

    Update custom_thumbnail.py

--- tobrot/plugins/custom_thumbnail.py
+++ tobrot/plugins/custom_thumbnail.py
@@ -44,8 +44,8 @@ async def save_thumb_nail(client, message):
         # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
         os.remove(downloaded_file_name)
         await ismgs.edit(
-            "Custom video / file thumbnail saved. " + \
-            "This image will be used in the upload, till clearthumbnail."
+            "✅️Custom Thumbnail Saved."
+           
         )
     else:
         await message.edit("Reply to a photo to save custom thumbnail")
