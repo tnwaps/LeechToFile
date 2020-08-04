643426f134fd69d70a40d7a316139d8199271cd1
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-04 10:01:46 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -43,7 +43,7 @@ async def progress_for_pyrogram(
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "〘{0}〙 of 〘{1}〙\nSpeed🚀: {2}/s\nETA: 〘{3}〙\n".format(
+        tmp = progress + "〘{0}〙 of 〘{1}〙\n**Speed**🚀: {2}/s\n**ETA:** 〘{3}〙\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
