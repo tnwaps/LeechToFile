e269e1f0e691789b572f4a75d7e559deab1d924a
tnwaps <65656829+tnwaps@users.noreply.github.com>
2020-12-08 13:17:56 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -6,7 +6,7 @@ class Config(object):
     # The Telegram API things
     APP_ID = int(os.environ.get("APP_ID"))
     API_HASH = os.environ.get("API_HASH")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 754150510))
+    OWNER_ID = int(os.environ.get("OWNER_ID",754150510))
     # Get these values from my.telegram.org
     # to store the channel ID who are authorized to use the bot
     AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
